8da5e410ef12a7e776b4f0071fa9759bd00d93a8
Nathan Andrade <nathan.nhdsk@gmail.com>
2024-04-16 19:47:59 -0300

    lab3: remove ideia de calcular média pelo script

--- lab3/diff.py
+++ lab3/diff.py
@@ -1,6 +1,3 @@
-def calcular_media(resultado):
-    pass
-
 def comparar_arquivos(arquivo1, arquivo2):
     with open(arquivo1, 'r') as f1, open(arquivo2, 'r') as f2:
         for linha1, linha2 in zip(f1, f2):
